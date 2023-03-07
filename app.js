c8986f3f57d2f9743d4e23e92ff573e5eebc6369
the-caped-crusaders-coding-apprentice <70288598+the-caped-crusaders-coding-apprentice@users.noreply.github.com>
2023-03-07 19:15:18 +0200

    api done

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ console.log(gameRow)
 let currentRow = 0
 let currentTile = 0
 let userWord = ""
+let targetWord = ""
 const enterButton = document.querySelector(".enter")
 const clearButton = document.querySelector(".clear")
 
-for (const item of letters){
+setTargetWord()
+
+for (const item of letters) {
     item.addEventListener("click", () => {
-        if (currentTile === 5){
+        if (currentTile === 5) {
             return
         }
         userWord += item.innerHTML
@@ -20,21 +23,64 @@ for (const item of letters){
     })
 }
 
-enterButton.addEventListener("click", () =>{
-    if (userWord.length !== 5){
+enterButton.addEventListener("click", () => {
+    if (userWord.length !== 5) {
         alert("Not enough letters")
     }
-    else{
-        userWord = "" // reset func
-        currentRow++
-        currentTile = 0
-        console.log("perform check")
+    else {
+        // const wordcheck = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${userWord}`)
+        //     .then(data => data.json())
+        //     .then(res => {
+        //         console.log(res)
+        //         if(Array.isArray(res)){
+        //             console.log("valid")
+        //         }
+        //         else{
+        //             alert("not a valid word")
+        //         }
+        //     })
+        checkUserWordWithApi()
+        resetUserWordAndRow()
     }
 })
 
-clearButton.addEventListener("click", () =>{
-    userWord = userWord.substring(0, userWord.length-1)
+clearButton.addEventListener("click", () => {
+    userWord = userWord.substring(0, userWord.length - 1)
     console.log(userWord)
     gameRow[currentRow].children[currentTile - 1].innerHTML = ""
-    currentTile-- 
+    currentTile--
 })
+
+async function checkUserWordWithApi() {
+    const wordcheck = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${userWord}`)
+    const data = await wordcheck.json()
+    console.log(data)
+    if (Array.isArray(data)) {
+        console.log("valid")
+    }
+    else {
+        alert("invalid word")
+    }
+}
+
+function resetUserWordAndRow() {
+    userWord = ""
+    currentRow++
+    currentTile = 0
+}
+
+async function setTargetWord() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '29d6a287cemsh3e2463647081f31p1218c2jsna4404faf76a8',
+            'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+        }
+    }
+
+    let target = await fetch('https://random-words5.p.rapidapi.com/getMultipleRandom?count=1&wordLength=5', options)
+
+    targetWord = await target.json()
+
+    console.log(targetWord)
+}
